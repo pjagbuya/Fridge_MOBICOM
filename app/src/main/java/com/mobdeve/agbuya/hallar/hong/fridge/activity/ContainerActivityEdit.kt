0626fb2fab49b382090712e38e86272ae0c765c5
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2025-07-14 18:30:04 +0800

    P7: Fragments final iteration

--- app/src/main/java/com/mobdeve/agbuya/hallar/hong/fridge/activity/ContainerActivityEdit.kt
+++ app/src/main/java/com/mobdeve/agbuya/hallar/hong/fridge/activity/ContainerActivityEdit.kt
@@ -4,22 +4,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mobdeve.agbuya.hallar.hong.fridge.R
 import com.mobdeve.agbuya.hallar.hong.fridge.R.id.container_activity_main_fragment
+import com.mobdeve.agbuya.hallar.hong.fridge.container.ContainerActivityEditFragment
 import com.mobdeve.agbuya.hallar.hong.fridge.container.ContainerActivityFragment
 
-class PaulMainActivity : AppCompatActivity() {
+class ContainerActivityEdit  : AppCompatActivity() {
 
-    private lateinit var  containerActivityFragment : ContainerActivityFragment
+    private lateinit var  containerActivityFragment : ContainerActivityEditFragment
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        containerActivityFragment = ContainerActivityEditFragment()
         setContentView(R.layout.main)
-
-
-        containerActivityFragment = ContainerActivityFragment()
         supportFragmentManager.beginTransaction().apply{
-            replace(container_activity_main_fragment, containerActivityFragment)
+            replace(R.id.container_activity_edit_fragment, containerActivityFragment)
             commit()
         }
+
 
 
     }
--- app/src/main/java/com/mobdeve/agbuya/hallar/hong/fridge/container/ContainerViewHolder.kt
+++ app/src/main/java/com/mobdeve/agbuya/hallar/hong/fridge/container/ContainerViewHolder.kt
@@ -1,12 +1,17 @@
 package com.mobdeve.agbuya.hallar.hong.fridge.container
 
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.agbuya.hallar.hong.fridge.R
-import com.mobdeve.agbuya.hallar.hong.fridge.atomicClasses.Container
+import com.mobdeve.agbuya.hallar.hong.fridge.domain.ContainerModel
 import com.mobdeve.agbuya.hallar.hong.fridge.databinding.ContainerComponentBinding
 
 class ContainerViewHolder(private val binding: ContainerComponentBinding): RecyclerView.ViewHolder(binding.root)  {
-    fun bindData(cont: Container){
+
+    val editBtn : Button = binding.editBtn
+    val deleteBtn : Button = binding.deleteBtn
+
+    fun bindData(cont: ContainerModel){
 
         cont.imageContainer.loadImageView(binding.containerIv)
         binding.fridgeNameTv.text = cont.name
