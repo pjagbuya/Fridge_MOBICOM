308d2bf638fab27abc4836d7dd503e5c912292fc
HooDue <letty_hong@dlsu.edu.ph>
2025-07-19 17:44:37 +0800

    added and connected fragment page to add recipe

--- app/src/main/java/com/mobdeve/agbuya/hallar/hong/fridge/activity/RecipeActivity.kt
+++ app/src/main/java/com/mobdeve/agbuya/hallar/hong/fridge/activity/RecipeActivity.kt
@@ -2,8 +2,10 @@ package com.mobdeve.agbuya.hallar.hong.fridge.activity
 
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.mobdeve.agbuya.hallar.hong.fridge.fragment.AddRecipeFragment
 import com.mobdeve.agbuya.hallar.hong.fridge.fragment.HeaderWithSearchBarFragment
 import com.mobdeve.agbuya.hallar.hong.fridge.R
 import com.mobdeve.agbuya.hallar.hong.fridge.fragment.ReusableRecyclerView
@@ -30,6 +32,11 @@ class RecipeActivity : AppCompatActivity() {
         loadHeader(HeaderWithSearchBarFragment())
         loadDynamicContent()
 
+        //recipe form
+        binding.addRecipeBtn.setOnClickListener {
+            openAddRecipeForm()
+        }
+
     }
 
     private fun loadHeader(fragment: Fragment) {
@@ -50,11 +57,42 @@ class RecipeActivity : AppCompatActivity() {
             .commit()
     }
 
-
-
     private fun loadContent(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
             .commit()
     }
+
+    private fun openAddRecipeForm() {
+        //hide the addbutton and text
+        binding.addRecipeBtn.visibility = View.GONE
+        binding.addContainerTv.visibility = View.GONE
+        // remove header
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.headerContainer, Fragment()) // Empty Fragment
+            .commit()
+        binding.headerContainer.visibility = View.GONE
+
+        // Load AddRecipeFragment in the dynamic container
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, AddRecipeFragment())
+            .addToBackStack(null) // so we can go back
+            .commit()
+    }
+
+    //return to recipe main page when pressing back
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+
+            // restore header and add button
+            loadHeader(HeaderWithSearchBarFragment())
+            binding.headerContainer.visibility = View.VISIBLE
+            binding.addRecipeBtn.visibility = View.VISIBLE
+            binding.addContainerTv.visibility = View.VISIBLE
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
+
