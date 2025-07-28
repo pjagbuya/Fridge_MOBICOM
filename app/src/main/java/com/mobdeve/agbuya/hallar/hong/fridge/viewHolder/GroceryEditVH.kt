2cee8ff7868e343791c468e07c08363365cbd183
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2025-07-28 17:28:28 +0800

    P20: Reorganized folders

---
+++ app/src/main/java/com/mobdeve/agbuya/hallar/hong/fridge/viewHolder/GroceryEditVH.kt
@@ -0,0 +1,4 @@
+package com.mobdeve.agbuya.hallar.hong.fridge.viewHolder
+
+class GroceryEditVH {
+}
--- app/src/main/java/com/mobdeve/agbuya/hallar/hong/fridge/viewModel/GroceryEditVH.kt
+++
@@ -1,4 +0,0 @@
-package com.mobdeve.agbuya.hallar.hong.fridge.viewModel
-
-class GroceryEditVH {
-}
